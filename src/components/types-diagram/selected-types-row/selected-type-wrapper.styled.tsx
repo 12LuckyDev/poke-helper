9d6868c625de947e903b559369d34103d96ec97b
12LuckyDev <12luckydev@gmail.com>
2021-11-02 14:56:47 +0100

    Responsiveness

--- src/components/types-diagram/selected-types-row/selected-type-wrapper.styled.tsx
+++ src/components/types-diagram/selected-types-row/selected-type-wrapper.styled.tsx
@@ -15,15 +15,5 @@ export const SelectedTypeWrapper = styled.div<SelectedTypeWrapperProps>`
 	font-size: 2.5rem;
 	text-transform: capitalize;
 	font-weight: bold;
-`;
-
-export const SelectedTypeIcoWrapper = styled.div`
-	height: 50px;
-	width: 50px;
-
-	svg {
-		height: 90%;
-		width: 90%;
-		margin: 5%;
-	}
+	flex: 1;
 `;
